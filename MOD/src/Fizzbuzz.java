46b614af6b103db6d6dfb783009cd9c3a2b0de76
zhiyuan.huang <zhiyuan.huang@MU-C1-14.mullen.pvt.k12.co.us>
2016-08-25 09:38:28 -0600

    Now you got to choose

--- MOD/src/Fizzbuzz.java
+++ MOD/src/Fizzbuzz.java
@@ -1,8 +1,37 @@
 import java.util.Scanner;
 public class Fizzbuzz
 	{
+		static Scanner userInput = new Scanner(System.in);
 
 		public static void main(String[] args)
+			{
+			while (true)
+				{
+				System.out.println("Type 1, 2, 3, or 4.");
+				int choice = userInput.nextInt();
+				if (choice > 4 || choice < 1)
+					{
+						System.out.println("1, 2, 3, and 4, you idiot!");
+					}
+				else if (choice == 1)
+					{
+						fibu();
+					}
+				else if (choice == 2)
+					{
+						tryNumber();
+					}
+				else if (choice == 3)
+					{
+						leapYear();
+					}
+				else
+					{
+						list();
+					}
+				}
+			}
+		public static void fibu()
 			{
 				for (int i = 1; i <= 100; i++)
 					{
@@ -23,8 +52,10 @@ public class Fizzbuzz
 								System.out.println(i);
 							}
 					}
+			}
+		public static void tryNumber()
+			{
 				System.out.println("Put a number.");
-				Scanner userInput = new Scanner(System.in);
 				int put = userInput.nextInt();
 				if (put % 2 == 0)
 					{
@@ -34,6 +65,9 @@ public class Fizzbuzz
 					{
 						System.out.println("Odd");
 					}
+			}
+		public static void leapYear()
+			{
 				System.out.println("Input a year");
 				int year = userInput.nextInt();
 				if (year % 4 == 0 && (year/100) % 4 == 0)
@@ -44,6 +78,9 @@ public class Fizzbuzz
 					{
 						System.out.println("Not leap");
 					}
+			}
+		public static void list()
+			{
 				int list[] = { 2, 5, -7, 1, 3, 12, 19, -16, 4 , 20};
 				for (int i = 0; i < list.length; i++)
 					{
@@ -53,5 +90,6 @@ public class Fizzbuzz
 							}
 					}
 			}
+			
 
 	}
